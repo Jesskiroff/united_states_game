592aa6295c00834a6e976e435c03c346ae119557
Jessica Kiroff <jesskiroff@gmail.com>
2026-03-01 23:11:24 -0500

    add while loop into game for continuous state guessing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # rbps ?
-
 import turtle
+import pandas
+
+
 screen = turtle.Screen()
 
 screen.title("US States Game")
@@ -15,5 +17,33 @@ turtle.shape(image)
 # turtle_coodinates = turtle.onscreenclick(get_coor_mouse_click)
 # turtle.mainloop()
 
-answer_state = screen.textinput(title = "Guess The State!", prompt = "What is another state?" )
+
+
+# with open("50_states.csv" ) as united_states:
+#     states_and_coordinates = united_states.read()
+
+# # print(states_and_coordinates)
+
+data = pandas.read_csv("50_states.csv")
+all_states= data.state.to_list()
+guessed_states = []
+
+while len (guessed_states) < 50:
+    state_input = screen.textinput(title = f"{len(guessed_states)} / 50 States Correct", prompt = "What is another state?").title()
+    
+
+#if answer state is one of the states in all th estates of the 50_states.csv
+    #if user gets it right:
+     #create a trtle to write the name of the state at the state's x and y coordinate
+
+if state_input in all_states:
+    guessed_states.append(state_input)
+    t = turtle.Turtle()
+    t.hideturtle()
+    t.penup()
+    state_data = data[data.state == state_input]
+    t.goto(int(state_data.x.item(), state_data.y.item()))
+    t.write(state_data.state.item())
+
+
 screen.exitonclick()
